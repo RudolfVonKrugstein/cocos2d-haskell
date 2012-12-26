91e952f6a749bcd40fddb6a85724168d8419a4fa
Nathan <nathan.huesken@posteo.de>
2012-12-26 01:01:50 +0100

    Added touch delegate

---
+++ src/Graphics/Cocos2d/TouchDelegate-ffi.hs
@@ -0,0 +1,35 @@
+module TouchDelegate where
+
+data Touch = Touch {
+               touchLocation :: (Double,Double),
+               touchPrevLocation :: (Double,Double),
+               touchViewLocation :: (Double,Double),
+               touchPrevViewLocation :: (Double,Double)
+             }
+
+-- Foreign type
+data TouchDelegateType
+type TouchDelegate = Ptr TouchDelegateType
+data ForeignTouchType
+type ForeignTouch = Ptr ForeignTouchType
+
+-- convert foreign touch
+foreign import jscall "%1.getLocation()"               touchGetLocation :: ForeignTouch -> IO Point
+foreign import jscall "%1.getPreviousLocation()"       touchGetPreviousLocation :: ForeignTouch -> IO Point
+foreign import jscall "%1.getLocationInView()"         touchGetLocationInView :: ForeignTouch -> IO Point
+foreign import jscall "%1.getPreviousLocationInView()" touchGetPreviousLocationInView :: ForeignTouch -> IO Point
+
+foreignTouchToTouch :: ForeignTouch -> IO Touch
+foreignTouchToTouch foreign = Touch <$> (pointToTuple <$> touchGetLocation foreign)
+                                    <*> (pointToTuple  <$> touchGetPreviousLocation foreign)
+                                    <*> (pointToTuple <$> touchGetLocationInView foreign)
+                                    <*> (pointToTuple <$> touchGetPreviousLocationInView foreign)
+
+class TouchDelegateDerived a where
+  toTouchDelegate :: a-> TouchDelegate
+
+  setOnTouchBegan :: a -> (Touch -> IO Bool) -> IO ()
+  setOnTouchBegan a f =
+    touchDelegateSetOnTouchBegan (toTouchDelegate a) (foreignTouchToTouch >>= f)
+
+foreign import jscall "%1.ccTouchBegan = function(t,_) {return A(%2,[[1,t],0])[3];}" touchDelegateSetOnTouchBegan TouchDelegate -> (ForeignTouch -> IO Bool) -> IO ()
