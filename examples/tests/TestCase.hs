ff2ff1d2f1abe464b9cd342b7903178a3ff92779
Nathan <nathan.huesken@posteo.de>
2012-12-30 17:17:21 +0100

    Added long forgotten test cases

---
+++ examples/tests/TestCase.hs
@@ -0,0 +1,45 @@
+module TestCase where
+
+import Graphics.Cocos2d
+import Graphics.Cocos2d.Scene
+import Resources
+
+-- Base structure to structure test cases
+data TestCase = TestCase {
+	prev     :: TestCase,
+	scene    :: IO Scene,
+	next     :: TestCase,
+	title    :: String}
+
+-- Create test case from list of scenes
+testCaseFromList :: [(IO Scene, String)] -> TestCase
+testCaseFromList all@((scene,name):rest) = TestCase prevTestCase scene nextTestCase name
+  where
+  prevTestCase = testCaseFromList $ (last all):(init all)
+  nextTestCase = testCaseFromList $ rest ++ [(scene,name)]
+
+runTestCase :: TestCase -> (Scene -> IO ()) -> IO ()
+runTestCase test modifyScene = do
+  (winWidth,winHeight) <- getWinSize
+  scene <- scene test
+  modifyScene scene
+
+  label <- createLabelTTF (title test) "Arial" 32
+  addChild scene label 1
+  setPosition label (winWidth/2.0, winHeight -50.0)
+
+  item1 <- createMenuItemImage s_pathB1 s_pathB2 (runTestCase (prev test) modifyScene)
+  item2 <- createMenuItemImage s_pathR1 s_pathR2 (runTestCase test modifyScene)
+  item3 <- createMenuItemImage s_pathF1 s_pathF2 (runTestCase (next test) modifyScene)
+
+  menu <- createMenuWithItems $ map (toMenuItem) [item1,item2,item3]
+
+  setPosition menu (0.0,0.0)
+  (width2,height2) <- getContentSize item2
+
+  setPosition item1 (winWidth /2.0 - width2 *2.0, height2 /2.0)
+  setPosition item2 (winWidth/2.0, height2 /2.0)
+  setPosition item3 (winWidth/2.0 + width2 * 2.0, height2 /2.0)
+
+  addChild scene menu 1
+  replaceScene scene
