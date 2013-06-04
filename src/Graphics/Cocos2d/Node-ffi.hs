e5b77b1007de690e26a05b38291236dfd2f136ba
Nathan <nathan.huesken@posteo.de>
2013-06-04 15:04:20 +0200

    started converting to new class system, starting with Node

--- src/Graphics/Cocos2d/Node-ffi.hs
+++ src/Graphics/Cocos2d/Node-ffi.hs
@@ -27,57 +27,60 @@ import Data.Functor
 import Data.Word
 import Graphics.Cocos2d.Utils
 
-{- Cocos2d is a object oriented framework. To map this to haskell, a similar
- - approach as wxHaskell has been choosen.
- - You can read about it here: http://www.haskell.org/haskellwiki/WxHaskell/Quick_start
- - The basic Idea is this:
- - Node a is of class "a" which is derived from Node. So Node () is the Node class itself.
- - That way, anthing that derives from note can be passed where a Node is expected.
+{- Cocos2d is a object oriented framework. To map this to haskell, type classes for every class
+ - in cocos2d are introduced. A derived class is part of the type class of its base class.
+ - A function "to<Base>" converts the derived class to the base class before calling a function of the base class.
+ - The haste-ffi-parser takes care of creating the appropriate functions, that converts a function taking a base class into
+ - a function, taking a member of the appropriate type class, and calling the convert function.
  -}
 
-data CNode a
-type Node a = Ptr (CNode a)
-data CNodeSet
-type NodeSet = Ptr CNodeSet
+data Node
+data NodeSet
 
-foreign import jscall "%1.addChild(%2,%3)"                addChild         :: Node a -> Node b -> Int -> IO ()
-foreign import jscall "%1.addChild(%2)"                   addChild_        :: Node a -> Node b -> IO ()
-foreign import jscall "%1.removeChild(%2)"                removeChild      :: Node a -> Node b -> IO ()
-foreign import jscall "%1.setTag(%2)"                     setTag           :: Node a -> Int -> IO ()
-foreign import jscall "%1.getChildByTag(%2)"              getChildByTag    :: Node a -> Int -> IO (Node ())
-foreign import jscall "%1.getParent()"                    getParent        :: Node a -> IO (Node ())
-foreign import jscall "%1.removeFromParent()"             removeFromParent :: Node a -> IO ()
-foreign import jscall "%1.setAnchorPoint(%2)"             jsSetAnchorPoint :: Node a -> Point -> IO ()
+class NodeDerived a where
+  toNode :: a -> Node
+
+instance NodeBase Node where
+  toNode = id
+
+foreign import cpattern "%1.addChild(%2,%3)"                addChild         :: (NodeBase a, NodeBase b) => a -> b -> Int -> IO ()
+foreign import cpattern "%1.addChild(%2)"                   addChild_        :: (NodeBase a, NodeBase b) => a -> b -> IO ()
+foreign import cpattern "%1.removeChild(%2)"                removeChild      :: (NodeBase a, NodeBase b) => a -> b -> IO ()
+foreign import cpattern "%1.setTag(%2)"                     setTag           :: (Nodebase a, NodeBase b) => a -> b -> Int -> IO ()
+foreign import cpattern "%1.getChildByTag(%2)"              getChildByTag    :: (NodeBase a) => a -> Int -> IO (Node ())
+foreign import cpattern "%1.getParent()"                    getParent        :: (NodeBase a) => a -> IO Node
+foreign import cpattern "%1.removeFromParent()"             removeFromParent :: (NodeBase a) => a -> IO ()
+foreign import cpattern "%1.setAnchorPoint(%2)"             jsSetAnchorPoint :: (NodeBase a) => a -> Point -> IO ()
 setAnchorPoint :: Node a -> (Double, Double) -> IO ()
 setAnchorPoint n p = tupleToPoint p >>= jsSetAnchorPoint n
-foreign import jscall "%1.setPosition(%2)"                jsSetPosition    :: Node a -> Point -> IO ()
+foreign import cpattern "%1.setPosition(%2)"                jsSetPosition    :: (NodeBase a) => a -> Point -> IO ()
 setPosition :: Node a -> (Double, Double) -> IO ()
 setPosition n p = tupleToPoint p >>= jsSetPosition n
-foreign import jscall "%1.getPosition()"                  jsGetPosition    :: Node a -> IO Point
+foreign import cpattern "%1.getPosition()"                  jsGetPosition    :: (NodeBase a ) => a -> IO Point
 getPosition :: Node a -> IO (Double, Double)
 getPosition n = jsGetPosition n >>= pointToTuple
-foreign import jscall "%1.setScale(%2,%3)"                setScale         :: Node a -> Double -> Double -> IO ()
-foreign import jscall "%1.setVisible(%2)"                 setVisible       :: Node a -> Bool -> IO ()
-foreign import jscall "%1.setColor(cc.c4b(%*))"           jsSetColor       :: Node a -> Word8 -> Word8 -> Word8 -> Word8 -> IO ()
+foreign import cpattern "%1.setScale(%2,%3)"                setScale         :: (NodeBase a) => a -> Double -> Double -> IO ()
+foreign import cpattern "%1.setVisible(%2)"                 setVisible       :: (NodeBase a) => a -> Bool -> IO ()
+foreign import cpattern "%1.setColor(cc.c4b(%*))"           jsSetColor       :: (NodeBase a) => a -> Word8 -> Word8 -> Word8 -> Word8 -> IO ()
 setColor :: Node a -> Color4b -> IO ()
 setColor n (Color4b r g b a) = jsSetColor n r g b a
-foreign import jscall "%1.setOpacity(%2)"                 setOpacity       :: Node a -> Double -> IO ()
-foreign import jscall "%1.setRotation(%2)"                setRotation      :: Node a -> Double -> IO ()
-foreign import jscall "%1.getContentSize()"               jsGetContentSize :: Node a -> IO Size
+foreign import cpattern "%1.setOpacity(%2)"                 setOpacity       :: (NodeBase a) => a -> Double -> IO ()
+foreign import cpattern "%1.setRotation(%2)"                setRotation      :: (NodeBase a) => a -> Double -> IO ()
+foreign import cpattern "%1.getContentSize()"               jsGetContentSize :: (NodeBase a) => a -> IO Size
 getContentSize :: Node a -> IO (Double,Double)
 getContentSize n = jsGetContentSize n >>= sizeToTuple
-foreign import jscall "%1.setContentSize(cc.size(%2,%3))" setContentSize :: Node a -> Double -> Double -> IO ()
-foreign import jscall "%1.schedule(function (a) {A(%2, [[1,a],0]);},%3)" scheduleInterval :: Node a -> IO () -> Double -> IO ()
-foreign import jscall "%1.scheduleOnce(function (a) {A(%2, [[1,a],0]);},%3)" scheduleOnce :: Node a -> IO () -> Double -> IO ()
-foreign import jscall "cc.Director.getInstance().getActionManager().resumeTarget(%1)" resumeTarget :: Node a -> IO ()
-foreign import jscall "cc.Director.getInstance().getActionManager().pauseTarget(%1)"  pauseTarget :: Node a -> IO ()
+foreign import cpattern "%1.setContentSize(cc.size(%2,%3))" setContentSize :: (NodeBase a) => a -> Double -> Double -> IO ()
+foreign import cpattern "%1.schedule(function (a) {A(%2, [[1,a],0]);},%3)" scheduleInterval :: (NodeBase a) => a -> IO () -> Double -> IO ()
+foreign import cpattern "%1.scheduleOnce(function (a) {A(%2, [[1,a],0]);},%3)" scheduleOnce :: (NodeBase a) => a -> IO () -> Double -> IO ()
+foreign import cpattern "cc.Director.getInstance().getActionManager().resumeTarget(%1)" resumeTarget :: (NodeBase a) => a -> IO ()
+foreign import cpattern "cc.Director.getInstance().getActionManager().pauseTarget(%1)"  pauseTarget  :: (NodeBase a) => a -> IO ()
 
 -- array of nodes
-nodeSetToNodeList :: NodeSet -> IO [Node ()]
+nodeSetToNodeList :: NodeSet -> IO [Node]
 nodeSetToNodeList set = do
   num <- nodeSetSize set
   mapM (\i -> nodeSetGetNthNode set i) [0..(num-1)]
 
-foreign import jscall "%1.length" nodeSetSize    :: NodeSet -> IO Int
-foreign import jscall "%1[%2]" nodeSetGetNthNode :: NodeSet -> Int -> IO (Node ())
+foreign import cpattern "%1.length" nodeSetSize    :: NodeSet -> IO Int
+foreign import cpattern "%1[%2]" nodeSetGetNthNode :: NodeSet -> Int -> IO (Node)
 
