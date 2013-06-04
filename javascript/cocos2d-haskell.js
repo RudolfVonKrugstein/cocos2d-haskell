c8da5eb9e32ec87f6600bddf1b4b0b6e2ddba903
Nathan <nathan.huesken@posteo.de>
2013-06-05 00:24:15 +0200

    same start context/ functions as new cocos2d-html5 version

--- javascript/cocos2d-haskell.js
+++ javascript/cocos2d-haskell.js
@@ -9,17 +9,39 @@ var cocos2dApp = cc.Application.extend({
 		cc.COCOS2D_DEBUG  = this.config['COCOS2D_DEBUG'];
 		cc.initDebugSetting();
 		cc.setup(this.config['tag']);
-		cc.Loader.getInstance().onloading = function () {
-			cc.LoaderScene.getInstance().draw();
-		};
-		cc.Loader.getInstance().onload = function () {
-			cc.AppController.shareAppController().didFinishLaunchingWithOptions();
-		};
-		cc.Loader.getInstance().preload(g_ressources);
+        cc.AppController.shareAppController().didFinishLaunchingWithOptions();
 	},
 	applicationDidFinishLaunching:function () {
-		this.startFun(this);
-		return true;
+      // initialize director
+      var director = cc.Director.getInstance();
+
+      var screenSize = cc.EGLView.getInstance().getFrameSize();
+      var resourceSize = cc.size(800, 450);
+      var designSize = cc.size(800, 450);
+
+      var searchPaths = [];
+      var resDirOrders = [];
+
+      searchPaths.push("res");
+      cc.FileUtils.getInstance().setSearchPaths(searchPaths);
+
+      cc.FileUtils.getInstance().setSearchResolutionsOrder(resDirOrders);
+
+      director.setContentScaleFactor(resourceSize.width / designSize.width);
+
+      cc.EGLView.getInstance().setDesignResolutionSize(designSize.width, designSize.height, cc.RESOLUTION_POLICY.SHOW_ALL);
+
+      // turn on display FPS
+      director.setDisplayStats(this.config['showFPS']);
+
+      // set FPS. the default value is 1.0/60 if you don't call this
+      director.setAnimationInterval(1.0 / this.config['frameRate']);
+
+      cc.LoaderScene.preload(g_ressources, function() {
+        this.startFun(this);
+      }, this);
+
+      return true;
 	}
 		
 });
